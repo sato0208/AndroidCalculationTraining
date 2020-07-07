f4e196b19e0eee11d1683b4d2bcd2aa9cfbb2e72
sato chiharu <kujira028@gmail.com>
2020-07-07 10:02:27 +0000

    [update]ボタンのクリック処理

--- app/src/main/java/com/example/calculationtraining/MainActivity.kt
+++ app/src/main/java/com/example/calculationtraining/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.calculationtraining
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+//    Todo 「スタート」ボタンを押したら
+//    Todo １． テスト画面を開く(Spinnerから選んだ問題数を渡す）
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+//            次の画面にいく選択した問題数を渡す
+        }
     }
 }
