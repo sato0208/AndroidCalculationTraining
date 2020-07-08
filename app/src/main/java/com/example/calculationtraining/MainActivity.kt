2ca1261f796a5fa3f75cce57d8bc8c6de7f8ee8a
sato chiharu <kujira028@gmail.com>
2020-07-08 08:05:50 +0000

    [update]タイトルの選択肢を表示する

--- app/src/main/java/com/example/calculationtraining/MainActivity.kt
+++ app/src/main/java/com/example/calculationtraining/MainActivity.kt
@@ -2,19 +2,26 @@ package com.example.calculationtraining
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-//    Todo 「スタート」ボタンを押したら
-//    Todo １． テスト画面を開く(Spinnerから選んだ問題数を渡す）
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        選択肢を入れるためのarrayadaptorをセット
+        val arrayAdapter = ArrayAdapter<int>(this, android.R.layout.simple_spinner_item)
+//        arrayAdapter.add(10)
+//        arrayAdapter.add(20)
+//        arrayAdapter.add(30)
 
+        val arrayAdapter = ArrayAdapter.createFromResource(this, R.array.number_of_question,android.R.layout.simple_spinner_item)
+        spinner.adapter = arrayAdapter
         button.setOnClickListener {
-//            次の画面にいく選択した問題数を渡す
+//    Todo 「スタート」ボタンを押したら
+//    Todo １． テスト画面を開く(Spinnerから選んだ問題数を渡す）
         }
     }
 }
