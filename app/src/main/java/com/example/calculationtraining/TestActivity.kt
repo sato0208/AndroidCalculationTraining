495bc506f7645f1987d54a0e430d8202fff3ac99
sato chiharu <kujira028@gmail.com>
2020-07-09 09:09:41 +0000

    [update]テスト画面大枠作成

--- app/src/main/java/com/example/calculationtraining/TestActivity.kt
+++ app/src/main/java/com/example/calculationtraining/TestActivity.kt
@@ -2,10 +2,58 @@ package com.example.calculationtraining
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_test.*
 
-class TestActivity : AppCompatActivity() {
+
+
+
+
+class TestActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+        //Todo テスト画面が開いたら
+
+        //Todo 「こたえ合わせ」ボタンが押されたら
+        buttonCheck.setOnClickListener {  }
+        //Todo 「もどる」ボタンが押されたら
+        buttonBack.setOnClickListener {
+
+            answerCheck()
+
+        }
+        //Todo 電卓ボタンが押されたら
+        button0.setOnClickListener(this)
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
+        button5.setOnClickListener(this)
+        button6.setOnClickListener(this)
+        button6.setOnClickListener(this)
+        button8.setOnClickListener(this)
+        button9.setOnClickListener(this)
+        buttonC.setOnClickListener(this)
+        buttonMinus.setOnClickListener(this)
+
+    //       一問目を出す
+        question()
+    }
+    // Todo 問題が出されたら（questionメソッド）
+    //    問題を処理するメソッド
+
+    private fun question() {
+        TODO("Not yet implemented")
+    }
+    //Todo こたえあわせ処理（answerCheckメソッド）
+    //    答え合わせ処理をするメソッド
+    private fun answerCheck() {
+        TODO("Not yet implemented")
+    }
+    //    ボタンが押されたときの処理
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
+
